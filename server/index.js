246202c0bcc1f6caa8a7e5e59dbccf0af775b3f9
tk-Hofman <97836781+tk-Hofman@users.noreply.github.com>
2022-02-21 18:08:57 +0400

    http://localhost:4000で定数tweetMessagesのオブジェクトを表示

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
+const tweetMessages = [
+  {message: "こんにちは", like: 2, id: "yosino"},
+  {message: "こんばんわ", like: 90, id: "toki"},
+  {message: "おはよう", like: 55, id: "jyo"},
+  {message: "さよなら", like: 30, id: "tbris"},
+  {message: "いただきます", like: 49, id: "oosaka"},
+  {message: "ご馳走様です", like: 31, id: "javascript"}
+];
+
+
 var http = require('http');
 var server = http.createServer((req,res) => {
-  res.end('Hello World');
+  res.writeHead(200, {'Content-Type' :'application/json'});
+  res.end(JSON.stringify(tweetMessages),'utf-8');
 });
 
 server.listen(4000);
