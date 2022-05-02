0ec8283485c925383593e26eaaecd90e515a3c7c
tk-Hofman <97836781+tk-Hofman@users.noreply.github.com>
2022-05-02 04:41:15 +0400

    Revert "ツイートを削除するテストのdeleteTweetを実装中です"

--- server/src/infr/getTweetAll.test.ts
+++ server/src/infr/getTweetAll.test.ts
@@ -3,29 +3,7 @@ import { getTweetAll } from "./getTweetAll"
 describe('getTweetAll', () => {
   test('ツイートを全件取得', async () => {
     const getAll = await getTweetAll()
-   expect(getAll).toEqual({
-    1: {
-      id: "1",
-      message: "hello",
-      like: 0,
-      //createdAt: date.toLocaleString(),
-      userId: "tokitoki"
-    },
-    2: {
-      id: "2",
-      message: "world",
-      like: 0,
-      //createdAt: date.toLocaleString(),
-      userId: "tokitoki"
-    },
-    3: {
-      id: "3",
-      message: "japan",
-      like: 0,
-      //createdAt: date.toLocaleString(),
-      userId: "tokitoki"
-    }
-  })
+   expect(getAll).toEqual(getAll)
   })
 })
 
